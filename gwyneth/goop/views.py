bbc4c3d816bc55b816011d51ef9a6a8bd33a1d3b
Imane Chafi <imanechafi@Imanes-MacBook-Pro.local>
2022-02-05 17:19:11 -0500

    camera test

--- gwyneth/goop/views.py
+++ gwyneth/goop/views.py
@@ -4,6 +4,19 @@ from django.http import HttpResponse
 
 from utils import get_db_handle
 
+from django.shortcuts import render
+from django.http.response import StreamingHttpResponse
+from goop.camera import VideoCamera
+# Create your views here.
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def video_stream(request):
+    return StreamingHttpResponse(gen(VideoCamera()),
+                    content_type='multipart/x-mixed-replace; boundary=frame')
+
 db = get_db_handle()
 
 def index(request):
