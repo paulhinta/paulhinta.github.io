74e7c5d5712918ce660278d3821cb71a68e622d0
paulhinta <paul_hinta@outlook.com>
2022-02-05 17:23:48 -0500

    About page

--- gwyneth/goop/views.py
+++ gwyneth/goop/views.py
@@ -10,8 +10,11 @@ def index(request):
     x = db.find({"listing_url":"https://www.airbnb.com/rooms/10006546"})
     
     context = {}
-    context['query'] = str(x[0])
+    #context['query'] = str(x[0])
     
     return render(request, 'home.html', context)
     #return HttpResponse(str(x[0]))
     #return HttpResponse("Gwyneth Paltrow flexes her Bitcoin knowledge as famed movie star and esteemed 'Goop' owner trumps the bald-headed Jeffrey Bezos in all-out Cryptocurrency war.")
+
+def about(request):
+    return render(request, 'about.html')
