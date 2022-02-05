c8c5758987ddd1b4f34324755bf126ddee2fc724
paulhinta <paul_hinta@outlook.com>
2022-02-05 12:39:23 -0500

    Connected to mongo

--- gwyneth/goop/views.py
+++ gwyneth/goop/views.py
@@ -1,6 +1,12 @@
+from gc import collect
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from utils import get_db_handle
+
+db = get_db_handle()
 
 def index(request):
-    return HttpResponse("Gwyneth Paltrow flexes her Bitcoin knowledge as famed movie star and esteemed 'Goop' owner trumps the bald-headed Jeffrey Bezos in all-out Cryptocurrency war.")
+    x = db.find({"listing_url":"https://www.airbnb.com/rooms/10006546"})
+    return HttpResponse(str(x[0]))
+    #return HttpResponse("Gwyneth Paltrow flexes her Bitcoin knowledge as famed movie star and esteemed 'Goop' owner trumps the bald-headed Jeffrey Bezos in all-out Cryptocurrency war.")
