681e491bf75d8787bdef09831854c88f9d273626
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2022-02-05 19:00:33 -0500

    CSS to resize image

--- gwyneth/goop/urls.py
+++ gwyneth/goop/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('video_stream', views.video_stream, name='video_stream'),
     path('about', views.about, name='about'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
